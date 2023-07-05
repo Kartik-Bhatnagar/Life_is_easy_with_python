90a08f5f9e142182766f994b991aa1b08e7a1fc0
KartikBhatnagar <kartikbhatnagar15@gmail.com>
2023-07-05 10:51:08 +0530

    simple Screenshot

---
+++ take_screenshot.py
@@ -0,0 +1,23 @@
+import pyautogui
+import time
+import os
+
+def create_folder(folder):
+    
+    #print(not os.path.isdir("screenshots"))
+    if not os.path.isdir(folder):
+        print("Created directory : ",folder)
+        os.mkdir(folder)
+
+
+def one_full_sshot(folder):
+    myScreenshot =  pyautogui.screenshot()
+    print(myScreenshot)
+    t = time.time()
+    file_name = ("".join(str(t).split("."))[0:14])+".png"
+    file_path = os.path.join(os.curdir,folder,file_name)
+    myScreenshot.save(file_path) 
+
+folder = "screenshots"
+create_folder(folder)
+one_full_sshot(folder)
